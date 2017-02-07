b1b680451d6a5f3905386b7cedef01c0c2c81539
Joseph Calabig <jcalabig@hotmail.com>
2017-02-06 21:36:54 -0800

    test

---
+++ config.py
@@ -0,0 +1,85 @@
+from constants import *
+
+
+queue_server = "localhost"
+username = "test"
+password = "test"
+
+alarm_actions = [
+    {
+        ACTION: SEND,
+        TYPE: COMMAND,
+        DEVICE: "camera",
+        OP_CODE: "capture video"
+    },
+    {
+        ACTION: SEND,
+        TYPE: COMMAND,
+        DEVICE: "buzzer1",
+        OP_CODE: "on"
+    }
+]
+
+home_monitor_config = [
+    {
+        STATE: BEGIN,
+        NEXT_STATE: NORMAL,
+        ACTIONS: [{
+                ACTION: SEND,
+                TYPE: COMMAND,
+                DEVICE: "kitchenLights1",
+                OP_CODE: "pauseEvents"
+            }]
+    },
+    {
+        STATE: ELEVATED,
+        CONDITION: "event != \"reset\"",
+        ACTIONS: [{
+            ACTION: SEND,
+            TYPE: COMMAND,
+            DEVICE: "buzzer1",
+            OP_CODE: "on"
+        }]
+    },
+    {
+        STATE: NORMAL,
+        NEXT_STATE: ELEVATED,
+        CONDITION: "event != \"reset\" and mode == \"away\"",
+        ACTIONS: alarm_actions
+    },
+    {
+        CONDITION: "event == \"panic\"",
+        NEXT_STATE: ELEVATED,
+        ACTIONS: alarm_actions
+    },
+    {
+        CONDITION: "event == \"reset\"",
+        NEXT_STATE: NORMAL,
+        ACTIONS: [
+            {
+                ACTION: SET_MODE_HOME
+            },
+            {
+                ACTION: SEND,
+                TYPE: COMMAND,
+                DEVICE: "kitchenLights1",
+                OP_CODE: "pauseEvents"
+            }
+        ]
+    },
+    {
+        CONDITION: "event == \"away\"",
+        NEXT_STATE: NORMAL,
+        ACTIONS: [
+            {
+                ACTION: SET_MODE_AWAY
+            },
+            {
+                ACTION: SEND,
+                TYPE: COMMAND,
+                DEVICE: "kitchenLights1",
+                OP_CODE: "resumeEvents"
+            }
+        ]
+    }
+]
