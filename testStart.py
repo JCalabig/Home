8f383dbddbe5cfba06b52d062a897116ae5e5ddd
Joseph Calabig <jcalabig@hotmail.com>
2017-02-09 21:35:17 -0800

    added test sender

---
+++ testStart.py
@@ -0,0 +1,13 @@
+from config import queue_server, username, password
+from constants import *
+from rabbitmq.Connection import Connection
+from rabbitmq.Sender import Sender
+
+connection = Connection(queue_server, username, password)
+sender = Sender(connection, "controller1", "events")
+sender.send({
+    TO: "controller1",
+    EVENT:"away"
+    })
+sender.un_initialize()
+connection.un_initialize()
